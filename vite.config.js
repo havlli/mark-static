50ec5f33086fee14e02e2dff7c57f29c753a21e2
havlli <havlliczech@gmail.com>
2024-07-12 21:09:00 +0200

    REFACTOR: change the name of the custom plugin to better reflect its functionality

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import generateDataPlugin from './vite-plugin-generate-data.js';
+import dataGenerator from './vite-plugin-generate-data.js';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss(), generateDataPlugin()]
+	plugins: [sveltekit(), purgeCss(), dataGenerator()]
 });
