5354cf1769d5d9c1cc83b55a5f25357207fbfd01
havlli <havlliczech@gmail.com>
2024-07-15 22:27:03 +0200

    adding global scoped styles for generated html

--- src/routes/[section]/[category]/[subcategory]/+layout.svelte
+++ src/routes/[section]/[category]/[subcategory]/+layout.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <article class="p-10 flex w-full justify-between gap-10 items-start">
-	<section class="space-y-4 mb-10 w-full">
+	<section class="max-w-[56rem] mx-auto space-y-4 mb-10 w-full">
 		<span class="badge variant-soft translate-y-1">{formatTitle(data.category)}</span>
 		<h1 class="h1">{formatTitle(data.subcategory)}</h1>
 		<div use:tocCrawler={{ mode: 'generate', scrollTarget: '#page', key: tocKey }}>
--- src/routes/[section]/[category]/[subcategory]/+page.svelte
+++ src/routes/[section]/[category]/[subcategory]/+page.svelte
@@ -1,13 +1,53 @@
 <script>
-		export let data;
+	export let data;
 </script>
 
-<section>
-		{@html data.content}
+<section class="page-content">
+	{@html data.content}
 </section>
 
 <style lang="postcss">
-	h1 {
-			@apply h2;
-	}
+    :global(.page-content > *) {
+        @apply my-4;
+    }
+
+    :global(.page-content > h1) {
+        @apply h1;
+    }
+
+    :global(.page-content > h2) {
+        @apply h2;
+    }
+
+    :global(.page-content > h3) {
+        @apply h3;
+    }
+
+    :global(.page-content > h4) {
+        @apply h4;
+    }
+
+    :global(.page-content > h5) {
+        @apply h5;
+    }
+
+    :global(.page-content > h6) {
+        @apply h6;
+    }
+
+    :global(.page-content > hr) {
+        @apply opacity-30;
+    }
+
+    :global(.page-content > ul) {
+        list-style-type: disc; /* Classic bullet points */
+        margin-left: 2rem; /* Indent the list */
+        padding-left: 0; /* Remove default padding */
+        font-family: sans-serif; /* A clean, readable font */
+        line-height: 1.6; /* Improve readability */
+    }
+
+    :global(.page-content > ul > li) {
+        margin-bottom: 0.5rem; /* Space between items */
+    }
 </style>
