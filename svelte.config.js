3539c61a26b768058a75539f1ec1813d8a2e88ee
havlli <havlliczech@gmail.com>
2024-08-03 20:35:34 +0200

    Update basePath configuration in svelte.config.js
    Restored the environment variable for basePath to allow dynamic setting. Removed the relative path configuration, as it led to inconsistencies in path resolution.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// const basePath = process.env.BASE_PATH || '';
-const basePath = '';
+const basePath = process.env.BASE_PATH || '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,8 +17,7 @@ const config = {
 			}
 		},
 		paths: {
-			base: basePath,
-			relative: true
+			base: basePath
 		}
 	}
 };
