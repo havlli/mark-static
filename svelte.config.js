683d287cb0fef3598714b9d724043b791784789a
havlli <havlliczech@gmail.com>
2024-08-03 20:59:21 +0200

    Enhance prerendering error handling in Svelte config
    Added a custom handler for HTTP errors during the prerendering process. This logs error details and ignores 404 errors while throwing exceptions for other status codes to aid in debugging.

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,14 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		prerender: {
-			handleMissingId: 'ignore'
+			handleMissingId: 'ignore',
+			handleHttpError: ({ status, path, referrer, referenceType }) => {
+				console.log(`HTTP Error: ${status} on path: ${path}, referrer: ${referrer}, type: ${referenceType}`);
+				if (status === 404) {
+					return; // Ignore 404 errors during prerendering
+				}
+				throw new Error(`${status} ${path} ${referenceType} from ${referrer}`);
+			}
 		},
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
