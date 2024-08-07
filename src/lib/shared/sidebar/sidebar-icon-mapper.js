fd5ad1df8998d9ac92c00d5d2bd9b4e05cb383c2
havlli <havlliczech@gmail.com>
2024-08-07 18:17:02 +0200

    Add icons for 'markdown' and 'other,' reclassify SvelteKit guides
    Added 'fa-solid fa-file-invoice' and 'fa-solid fa-otter' icons to sidebar-icon-mapper.js. Reorganized SvelteKit guides by moving them from the "Guides" to the "Other" category.

--- src/lib/shared/sidebar/sidebar-icon-mapper.js
+++ src/lib/shared/sidebar/sidebar-icon-mapper.js
@@ -1,5 +1,7 @@
 const iconMap = new Map([['default', 'fa-solid fa-book']]);
 iconMap.set('info', 'fa-solid fa-info');
+iconMap.set('markdown', 'fa-solid fa-file-invoice');
+iconMap.set('other', 'fa-solid fa-otter');
 
 export const mapSectionToIcon = (sectionName) => {
 	if (!sectionName || !iconMap.has(sectionName.toLowerCase())) {
