207249e55f9afad49a1e2f8816eeac861f136c7b
havlli <havlli@users.noreply.github.com>
2024-08-07 16:02:40 +0000

    deploy: b5f9fd30551196d33698bd1682e615ceaa6729a3

---
+++ _app/immutable/chunks/paths.Cn9I8X-4.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1r7c08h)==null?void 0:s.base)??"/mark-static";var a;const e=((a=globalThis.__sveltekit_1r7c08h)==null?void 0:a.assets)??t;export{e as a,t as b};
--- _app/immutable/entry/start.BY0sxd3-.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"../chunks/entry.BSNJv8bq.js";export{a as start};
